81cb9ed79ae51eed5b1b8eb446036fb751a7685b
Autopawn <franciscojacb@gmail.com>
2018-12-10 15:38:19 -0300

    Added qsort usage to example.

--- example.c
+++ example.c
@@ -17,24 +17,38 @@ CADTS_HEAP(intheap,int,A<B)
 CADTS_HASHTABLE(loloctable,unsigned long long,char,A==B,(A>>32)^(A))
 
 
+int cmp_floats(const void *a, const void *b){
+    return (*(float*)a-*(float*)b);
+}
+
+
 int main(int argc, char const *argv[]){
     //
     //
     printf("VECTOR:\n");
     floatvec *fvec = floatvec_init(0);
     for(int i=0;i<20;i++){
-        floatvec_endadd(fvec,i);
+        floatvec_endadd(fvec,20-3*i-2*(i%2));
     }
     for(int i=0;i<10;i++){
         floatvec_endpop(fvec);
     }
+    // Print vector
     for(int i=0;i< floatvec_len(fvec);i++){
         // Access by "items" array, this can only be done with vectors.
-        printf("%f ",floatvec_item(fvec, i));
+        printf("%f ",fvec->items[i]);
     }
     printf("\n");
+    // Sort vector acceding by array
+    qsort(fvec->items,floatvec_len(fvec),sizeof(float),cmp_floats);
+    // Print vector
+    for(int i=0;i< floatvec_len(fvec);i++){
+        // Access by "items" array, this can only be done with vectors.
+        printf("%f ",fvec->items[i]);
+    }
+    printf("\n");
+    // Free vector
     floatvec_free(fvec);
-    //
     //
     printf("HEAP:\n");
     intheap *heap = intheap_init(0);
